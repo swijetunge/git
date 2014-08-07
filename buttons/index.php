19fa509dba6b3428abec452fb0be77857eba1116
sandun wijetunge <sandunme@hotmail.com>
2014-08-07 11:20:54 +0100

    CSS Buttons
    <input>, <a> and <button> css

---
+++ buttons/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CSS Buttons</title>
+		<link rel="stylesheet" href="style.css" />
+	</head>
+	<body>
+	
+		<a href="" class="button button-primary button-tiny">Sign up</a>
+		<a href="" class="button button-primary">Sign up</a>
+		<a href="" class="button button-primary button-medium">Sign up</a>
+		<a href="" class="button button-primary button-large">Sign up</a>
+		
+		<hr />
+		
+		<input type="submit" value="Sign up" class="button button-yellow button-tiny" />
+		<input type="submit" value="Sign up" class="button button-yellow" />
+		<input type="submit" value="Sign up" class="button button-yellow button-medium" />
+		<input type="submit" value="Sign up" class="button button-yellow button-large" />
+		
+		<hr />
+		
+		<button  class="button button-purple button-tiny" >Sing up</button>
+		<button  class="button button-purple" >Sing up</button>
+		<button  class="button button-purple button-medium" >Sing up</button>
+		<button  class="button button-purple button-large" >Sing up</button>
+	</body>
+</html>
