2ca15d19529ee15d8732086fc2438faf64d9375d
sandun wijetunge <sandunme@hotmail.com>
2014-09-17 14:35:19 +0100

    spinner
    pure CSS spinner

---
+++ spinner/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<link type="text/css" rel="stylesheet" href="style.css">
+	</head>
+	<body>
+	  	<h3>Spinner</h3>
+	  	<div class="spinner"></div>
+	</body>
+</html>
