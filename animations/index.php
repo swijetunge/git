efa2fe4adb7e389fbbad3c893b8d865de7eccd09
sandun wijetunge <sandunme@hotmail.com>
2014-10-01 16:56:18 +0100

    animation
    swing bulbs

---
+++ animations/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Animation Bulbs</title>
+        
+		<link type="text/css" rel="stylesheet" href="styles.css" />
+	</head>
+	<body>
+		
+		<div class="image-wrap small">
+		  <div class="background"></div>
+		  <div class="bulb1"></div>
+		  <div class="bulb2"></div>
+		  <div class="bulb3"></div>
+		  <div class="bulb4"></div>
+		  <div class="bulb">Bulbs</div>
+		  <div class="me">Sandun</div>
+		</div>
+
+	</body>
+</html>
