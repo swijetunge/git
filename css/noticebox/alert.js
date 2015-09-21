624cdd4c47660834c0a9393721ee7634154c0d85
sandun wijetunge <sandunme@hotmail.com>
2015-09-21 16:45:12 +0100

    css stuff

---
+++ css/noticebox/alert.js
@@ -0,0 +1,16 @@
+(function($) {
+	$.fn.alert = function() {
+		return this.each(function() {
+			var self = $(this);
+			
+			self.on('click', '.close', function() {
+				self.addClass('close')
+			});
+			
+			self.on('transitionend webkitTransitionEnd oTransitionEnd', function() {
+				self.remove();
+			});
+		
+		});
+	};
+}(jQuery))
