11f030b8b8ae6a5f7479506b161d8fc89db20b5c
Sandun Wijetunge <sandun.wijetunge@salary-exchange.co.uk>
2018-05-01 09:46:47 +0100

    added css overlay

---
+++ css/overlay/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div id="home">
+        <div class="wallpaper"></div>
+        <div class="info-wrapper">
+            <span class="name">Sandun Wije</span>
+        </div>
+    </div>
+</body>
+</html>
